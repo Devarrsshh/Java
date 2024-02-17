d5cf4dc3bb335739e81eae404fc5d3912239c224
Devarsh <129305335+Devarrsshh@users.noreply.github.com>
2024-02-17 14:01:56 +0530

    Add files via upload

---
+++ src/CWH_16_conditionals.java
@@ -0,0 +1,12 @@
+public class CWH_16_conditionals {
+    public static void main(String[] args) {
+        int age = 18;
+        boolean cond = (age == 18);
+
+        if (cond) {
+            System.out.println("Yes boy you can drive");
+        } else {
+            System.out.println("No boy you cannot drive");
+        }
+    }
+}
