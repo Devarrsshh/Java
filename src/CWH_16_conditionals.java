f5083475c9ac3c5b61b581072f517be303e1a55f
Devarsh <devarshmeh19@gmail.com>
2024-02-17 18:36:11 +0530

    conditionals

---
+++ src/CWH_16_conditionals.java
@@ -0,0 +1,12 @@
+public class CWH_16_conditionals {
+    public static void main(String[] args) {
+
+        int a = 29;
+        if (a>18) {
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("you are underage!");
+        }
+    }
+}
