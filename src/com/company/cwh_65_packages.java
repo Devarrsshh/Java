49a675159503ff05ed8ff6771d07d14eb5e9525e
Devarsh <devarshmeh19@gmail.com>
2024-03-18 21:56:35 +0530

    Creating a Thread by Extending Thread class

---
+++ src/com/company/cwh_65_packages.java
@@ -0,0 +1,10 @@
+package com.company;
+
+//import java.util.Scanner;
+import java.util.*;
+public class cwh_65_packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("this is my scanner");
+    }
+}
