1d0f2d8e7ef9fee6ef9d6e42524ddd9a7f5114b5
Devarsh <devarshmeh19@gmail.com>
2024-03-22 10:38:54 +0530

    learned about thread_priorities

--- src/com/company/ProblemSolvingGround.java
+++ src/com/company/ProblemSolvingGround.java
@@ -1,8 +1,7 @@
 package com.company;
-
+class Mythread extends Thread {
+}
 public class ProblemSolvingGround {
     public static void main(String[] args) {
-        System.out.println( -5 + 8 * 6);
-        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
     }
 }
